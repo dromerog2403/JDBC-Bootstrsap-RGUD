19a44c227ba9230868d25b6dbb1e29e94d815d6c
gerdoc <ggerdoc@gmail.com>
2026-09-17 08:56:20 -0600

    versiÃón con base

--- src/main/java/org/gerdoc/MiServlet.java
+++ src/main/java/org/gerdoc/MiServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serial;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet("/MiServlet")
 public class MiServlet extends HttpServlet
@@ -16,17 +20,55 @@ public class MiServlet extends HttpServlet
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        String url = "jdbc:mysql://127.0.0.1/TEST?user=root&password=1234";
+        String sql = "INSERT INTO ROL_Usuario(ROL_ROL,Usuario_USUARIO) VALUES(?,?)";
+        int row = 0;
         resp.setContentType("text/html;charset=UTF-8");
 
-        PrintWriter out = resp.getWriter();
 
+        PrintWriter out = resp.getWriter();
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection( url );
+            preparedStatement = connection.prepareStatement(sql);
+            if( preparedStatement == null )
+            {
+                return;
+            }
+            preparedStatement.setString(1, req.getParameter("exampleInputEmail1") );
+            preparedStatement.setString(2, req.getParameter("exampleSelect") );
+            row = preparedStatement.executeUpdate( );
+
+            System.out.println( connection.isClosed( ) );
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (InstantiationException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (IllegalAccessException e)
+        {
+            throw new RuntimeException(e);
+        }
         out.println("<html>");
         out.println("<body>");
         out.println("<h1>Hola desde Servlet</h1>");
         out.println("<p>Tomcat 9 + IntelliJ + Maven</p>");
         out.println("<p>Valor del parámetro 'correo': " + req.getParameter("exampleInputEmail1") + "</p>");
         out.println("<p>Valor del parámetro 'exampleSelect': " + req.getParameter("exampleSelect") + "</p>");
+        out.println("<p>Row': " + row + "</p>");
 
 
         out.println("</body>");
